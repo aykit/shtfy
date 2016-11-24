3e4ebffdd5a047e227c6f5ade44a618e43400a02
m <maltefiala@users.noreply.github.com>
2016-11-24 20:06:51 +0100

    added ENV var support
    ... quick and dirty ... I guess

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
 
-define("DB_DSN", "mysql:host=127.0.0.1;dbname=shtfy");
-define("DB_USER", "shtfy");
-define("DB_PASSWORD", "");
+define("DB_DSN", getenv("SHTFY_DB_DSN") ||
+       "mysql:host=127.0.0.1;dbname=shtfy");
+define("DB_USER", getenv("SHTFY_DB_USER") || "shtfy");
+define("DB_PASSWORD", getenv("SHTFY_DB_PASSWORD") || "");
 
 ?>
