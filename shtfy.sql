f3ae6e3fe09af95adca868a5b49f55ae2ff207e0
Friedrich Schöller <code@schoeller.se>
2019-03-03 17:59:15 +0100

    Add PostgreSQL initialization

--- shtfy.sql
+++ shtfy.sql
@@ -1,6 +1,14 @@
+-- MySQL
 CREATE TABLE `shtfy` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `url` text NOT NULL,
   `hits` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- PostgreSQL
+CREATE TABLE shtfy (
+  id serial NOT NULL PRIMARY KEY,
+  url text NOT NULL,
+  hits int NOT NULL DEFAULT 0
+);
